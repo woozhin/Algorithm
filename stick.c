4e87abcc59e9509f2c8eda8076ad0047205d47df
woozhin <A20213228@HackerLogin-gcc.8.8.8.8>
2024-04-17 17:42:32 +0900

    Commit

---
+++ stick.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    int x;
+    int n = 64;
+    int sum = 0;
+
+    scanf("%d", &x);
+
+    for(i=0;;i++){
+        n / 2
+        if(x>=n) sum += n;
+        printf("%d", sum);
+    }
+    return 0;
+}
